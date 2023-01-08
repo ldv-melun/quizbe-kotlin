67da41f46d3c14fa434d70d65093251b1d92de4a
Olivier Capuozzo <olivier.capuozzo@gmail.com>
2023-01-08 10:23:43 +0100

    corr comment bug in _application.properties

--- src/main/kotlin/org/quizbe/config/PopulateData.kt
+++ src/main/kotlin/org/quizbe/config/PopulateData.kt
@@ -1,7 +1,6 @@
 package org.quizbe.config
 
 import org.quizbe.dto.*
-import org.quizbe.exception.TopicNotFoundException
 import org.quizbe.exception.UserNotFoundException
 import org.quizbe.model.Rating
 import org.quizbe.model.Role
@@ -18,36 +17,27 @@ import java.time.LocalDateTime
 
 @Order(value = 1)
 @Component
-class PopulateData : ApplicationRunner {
+class PopulateData @Autowired constructor(
+    private val userService: UserService,
+    private val roleService: RoleService,
+    private val topicService: TopicService,
+    private val ratingService: RatingService,
+    private val questionService: QuestionService
+) : ApplicationRunner {
+
     var logger = LoggerFactory.getLogger(PopulateData::class.java)
 
-    private var admin: User? = null
-    private var teacher: User? = null
-    private var eleve: User? = null
-
-    @Autowired
-    var userService: UserService? = null
-
-    @Autowired
-    var roleService: RoleService? = null
-
-    @Autowired
-    var topicService: TopicService? = null
-
-    @Autowired
-    var questionService: QuestionService? = null
-
-    @Autowired
-    var ratingService: RatingService? = null
-
+    private lateinit var admin: User
+    private lateinit var teacher: User
+    private lateinit var eleve: User
 
     @Throws(Exception::class)
     override fun run(args: ApplicationArguments) {
-        val role = roleService!!.findByName("USER")
+        val role = roleService.findByName("USER")
         if (role != null) return
-        roleService!!.saveRole(Role("USER"))
-        roleService!!.saveRole(Role("TEACHER"))
-        roleService!!.saveRole(Role("ADMIN"))
+        roleService.saveRole(Role("USER"))
+        roleService.saveRole(Role("TEACHER"))
+        roleService.saveRole(Role("ADMIN"))
         createUsers()
         addTopics()
         addQuestions()
@@ -55,57 +45,57 @@ class PopulateData : ApplicationRunner {
     }
 
     private fun addRatings() {
-        val question1 = questionService!!.findById(1)
+        val question1 = questionService.findById(1)
         var rating = Rating(null, "La vie est belle", 5, LocalDateTime.now(), question1, teacher)
-        ratingService!!.save(rating)
+        ratingService.save(rating)
         rating = Rating(null, "La vie, cette inconnue", 3, LocalDateTime.now(), question1, eleve)
-        ratingService!!.save(rating)
+        ratingService.save(rating)
     }
 
     private fun addTopics() {
         var topicDto = TopicDto("Topic1")
 
-        topicDto.creatorUsername = teacher!!.username
+        topicDto.creatorUsername = teacher.username
         val scopeDtos: MutableList<ScopeDto> = ArrayList()
         scopeDtos.add(ScopeDto("scope1"))
         scopeDtos.add(ScopeDto("scope2"))
         topicDto.setScopesDtos(scopeDtos)
-        topicService!!.saveTopicFromTopicDto(topicDto, null)
+        topicService.saveTopicFromTopicDto(topicDto, null)
         topicDto = TopicDto()
         topicDto.name = "Topic2"
-        topicDto.creatorUsername = teacher!!.username
+        topicDto.creatorUsername = teacher.username
         scopeDtos.add(ScopeDto("scope3"))
         topicDto.setScopesDtos(scopeDtos)
-        topicService!!.saveTopicFromTopicDto(topicDto, null)
+        topicService.saveTopicFromTopicDto(topicDto, null)
 
         // users subscribe to topic1
-        val topic = topicService!!.findTopicById(1L).get()
+        val topic = topicService.findTopicById(1L).get()
 //        admin!!.subscribedTopics.add(topic)
-        topic.addSubscribedr(admin!!)
-        topic.addSubscribedr(teacher!!)
-        topic.addSubscribedr(eleve!!)
+        topic.addSubscribedr(admin)
+        topic.addSubscribedr(teacher)
+        topic.addSubscribedr(eleve)
 
-        topicService!!.save(topic)
+        topicService.save(topic)
 
     }
 
     private fun addQuestions() {
-        val topic = topicService!!.getTopicById(1L).get()
+        val topic = topicService.getTopicById(1L).get()
 
         val responseDtos = mutableListOf<ResponseDto>()
         responseDtos.add(ResponseDto(null, "_42_", "feedback proposition 1", 1))
         responseDtos.add(ResponseDto(null, "proposition 2", "feedback proposition 2", -1))
         responseDtos.add(ResponseDto(null, "proposition 3", "feedback proposition 3", -2))
-        val questionDto = QuestionDto(null, topic, 1L, admin!!.username)
+        val questionDto = QuestionDto(null, topic, 1L, admin.username)
         questionDto.name = "Question1"
         questionDto.sentence = "Answer to the Ultimate Question of Life, the Universe, and Everything"
         questionDto.responseDtos = responseDtos
-        questionService!!.saveQuestionFromQuestionDto(questionDto)
+        questionService.saveQuestionFromQuestionDto(questionDto)
     }
 
     @Throws(SQLIntegrityConstraintViolationException::class)
     private fun createUsers() {
-        val userAdmin = userService!!.findById(1).orElse(null)
+        val userAdmin = userService.findById(1).orElse(null)
         if (userAdmin == null) {
             val roles: MutableSet<String> = HashSet()
             roles.add("USER")
@@ -113,19 +103,19 @@ class PopulateData : ApplicationRunner {
             roles.add("ADMIN")
             val adminDto = UserDto("admin", "admin@admin.org", "adminadmin", roles)
             logger.info("userAdmin : $adminDto")
-            admin = userService!!.saveUserFromUserDto(adminDto)
+            admin = userService.saveUserFromUserDto(adminDto)
             roles.remove("ADMIN")
             val teacherDto = UserDto("prof", "prof@prof.org", "profprof", roles)
             logger.info("userTeacher : $teacherDto")
-            teacher = userService!!.saveUserFromUserDto(teacherDto)
+            teacher = userService.saveUserFromUserDto(teacherDto)
 
             roles.remove("TEACHER")
             val eleveDto = UserDto("eleve", "eleve@eleve.org", "eleveeleve", roles)
             logger.info("userEleve : $eleveDto")
-            eleve = userService!!.saveUserFromUserDto(eleveDto)
+            eleve = userService.saveUserFromUserDto(eleveDto)
         }
-        this.admin = userService!!.findByUsername("admin") ?: throw UserNotFoundException("admin not found")
-        this.teacher = userService!!.findByUsername("prof") ?: throw UserNotFoundException("prof not found")
-        this.eleve = userService!!.findByUsername("eleve") ?: throw UserNotFoundException("eleve not found")
+        this.admin = userService.findByUsername("admin") ?: throw UserNotFoundException("admin not found")
+        this.teacher = userService.findByUsername("prof") ?: throw UserNotFoundException("prof not found")
+        this.eleve = userService.findByUsername("eleve") ?: throw UserNotFoundException("eleve not found")
     }
 }
